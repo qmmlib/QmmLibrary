5adad32abbe33569db6c15a0fa1c89522ba31a95
qmmlib <qmmlib@gmail.com>
2012-11-30 23:12:33 +0200

    qmmchat update to compile in windows

--- qmmchat.h
+++ qmmchat.h
@@ -44,9 +44,8 @@ If chat_level > 3, a test_file should be passed so test accuracy can be calculat
 if (param->chat_level > 0) {																																			\
 	printf("Iter\t%d\tCPU_time\t%.10f\t", iter, cpu_time_elapsed);																									\
 	if (param->chat_level > 1) {																																		\
-        /* printf("|w|_inf\t%.4f\t|diff_w|_inf\t%.4f\t",wmax_new,Dmax_new); */                                                                                         	\ 
+        /* printf("|w|_inf\t%.4f\t|diff_w|_inf\t%.4f\t",wmax_new,Dmax_new); */                                                                                         	\
 		if (param->chat_level > 2) {																																	\
-			/* printf("Loss type\t%d\tcurvature type\t%d\tReg_param\t%.1e\tloss_param:\t%.1e\tcurv_param\t%.1e\t",loss_type, curv_type, reg_param, loss_param, curv_param);*/	\
 			printf("Objective_value\t%lf\t", objective_value);																										\
 			if (param->chat_level > 3) {																																\
 				if(param->test_file==0)																																\
